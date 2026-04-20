2ada4ca4c9ac829f65eaf39a7f44b4c7ac0794db
ivan.marroquin <bryan.marroquin@coop1.com.sv>
2026-04-20 08:48:19 -0600

    Cambios para catalogo

---
+++ src/main/java/com/empresa/tomaturno/framework/adapters/output/persistencia/entity/CatalogoDetalleJpaEntity.java
@@ -0,0 +1,45 @@
+package com.empresa.tomaturno.framework.adapters.output.persistencia.entity;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "catalogodeta", schema = "tomaturno")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CatalogoDetalleJpaEntity implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @EmbeddedId
+    private CatalogoDetallePKJpaEntity id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "descripcion", nullable = false, length = 250)
+    private String descripcion;
+
+    @Column(name = "usuario_creacion", nullable = false, length = 50)
+    private String usuarioCreacion;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "usuario_modificacion", length = 50)
+    private String usuarioModificacion;
+
+    @Column(name = "fecha_modificacion")
+    private LocalDateTime fechaModificacion;
+
+    @Column(name = "estado", nullable = false)
+    private Integer estado;
+}
