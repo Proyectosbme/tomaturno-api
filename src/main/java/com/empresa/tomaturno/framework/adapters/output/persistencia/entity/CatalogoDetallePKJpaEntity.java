2dc38bf50176093103a0a8ede03d70dc948b1e42
ivan.marroquin <bryan.marroquin@coop1.com.sv>
2026-04-20 08:48:19 -0600

    Cambios para catalogo

---
+++ src/main/java/com/empresa/tomaturno/framework/adapters/output/persistencia/entity/CatalogoDetallePKJpaEntity.java
@@ -0,0 +1,25 @@
+package com.empresa.tomaturno.framework.adapters.output.persistencia.entity;
+
+import java.io.Serializable;
+
+
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class CatalogoDetallePKJpaEntity implements Serializable {
+
+    private Long idCatalogo;
+    private Long idDetalle;
+
+}
---
+++ src/main/java/com/empresa/tomaturno/framework/adapters/output/persistencia/entity/CatalogoJpaEntity.java
@@ -0,0 +1,43 @@
+package com.empresa.tomaturno.framework.adapters.output.persistencia.entity;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "catalogo ", schema = "tomaturno")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CatalogoJpaEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "descripcion", nullable = false, length = 250)
+    private String descripcion;
+
+    @Column(name = "usuario_creacion", nullable = false, length = 50)
+    private String usuarioCreacion;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "usuario_modificacion", length = 50)
+    private String usuarioModificacion;
+
+    @Column(name = "fecha_modificacion")
+    private LocalDateTime fechaModificacion;
+
+    @Column(name = "estado", nullable = false)
+    private Integer estado;
+}
