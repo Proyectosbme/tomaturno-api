b4f3b94eee033051693bc2a2c2e7da0244b8a071
ivan.marroquin <bryan.marroquin@coop1.com.sv>
2026-04-21 13:57:43 -0600

    Implementacion de casos de uso en service command

--- src/main/java/com/empresa/tomaturno/catalogos/application/command/service/CatalogoCommandService.java
+++ src/main/java/com/empresa/tomaturno/catalogos/application/command/service/CatalogoCommandService.java
@@ -1,23 +1,29 @@
 package com.empresa.tomaturno.catalogos.application.command.service;
 
 import com.empresa.tomaturno.catalogos.application.command.port.input.CatalogoCommandInputPort;
+import com.empresa.tomaturno.catalogos.application.command.usecase.CrearCatalogoDetalleUseCase;
+import com.empresa.tomaturno.catalogos.application.command.usecase.CrearCatalogoUseCase;
 import com.empresa.tomaturno.catalogos.dominio.entity.Catalogo;
 import com.empresa.tomaturno.catalogos.dominio.entity.CatalogoDetalle;
 
 public class CatalogoCommandService  implements CatalogoCommandInputPort {
+    private final CrearCatalogoDetalleUseCase crearCatalogoDetalleUseCase;
+    private final CrearCatalogoUseCase crearCatalogoUseCase;
+
+    public CatalogoCommandService(CrearCatalogoDetalleUseCase crearCatalogoDetalleUseCase, 
+        CrearCatalogoUseCase crearCatalogoUseCase) {
+        this.crearCatalogoDetalleUseCase = crearCatalogoDetalleUseCase;
+        this.crearCatalogoUseCase = crearCatalogoUseCase;
+    }
 
     @Override
     public Catalogo crear(Catalogo catalogo, String usuario) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crear'");
+        return crearCatalogoUseCase.ejecutar(catalogo, usuario);
     }
 
     @Override
     public Catalogo crearDetalle(Long idCatalogo, CatalogoDetalle detalle, String usuario) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crearDetalle'");
-    }
-    
+        return crearCatalogoDetalleUseCase.ejecutar(idCatalogo, detalle, usuario);
+    }   
    
-    
 }
